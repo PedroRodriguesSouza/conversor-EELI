c6732691cad05a184a05c1d15884f0ca6b438eda
Pedro Rodrigues Souza <55005350+PedroRodriguesSouza@users.noreply.github.com>
2022-04-10 22:34:42 -0300

    "implementando funcionalidade: conversor float"

--- conversor.cpp
+++ conversor.cpp
@@ -2,43 +2,71 @@
 
 using namespace std;
 
-void conversor_generico(int n, int base);
+void conversor_generico(float n, int base);
+void conversor_float(float n, int base);
 
 int main()
 {
-     int n;
+    float n;
 
     cout << "Digite um numero: ";
     cin >> n;
 
-    conversor_generico(n, 2);
+    conversor_generico(n,2);
 
     return 0;
 }
 
-void conversor_generico(int n, int base)
+void conversor_generico(float n, int base)
 {
     int resto;
+    float f = n - int(n);
+    int i = int(n);
 
     if(base < 11)
     {
-        while(n > base - 1)
-        {
-            resto = n % base;
-            cout << resto;
-            n = (n - resto) / base;
-        }
-        cout << n << endl << endl;
+        resto = i % base;
+        i = (i - resto) / base;
+        if(i)
+            conversor_generico(i, base);
+        cout << resto;
     }
+
     else
     {
         char lista[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        while(n > base - 1)
+        resto = i % base;
+        i = (i - resto) / base;
+
+        if(i)
+            conversor_generico(n, base);
+            
+        cout << lista[resto];
+    }
+
+    if(f)
+    {
+        cout << ",";
+        conversor_float(f, base);
+        cout << "\n\n";
+    }
+}
+
+void conversor_float(float n, int base)
+{
+    int aux;
+    int cont = 0;
+    while(n != 0 && cont < 8)
+    {
+        n *= base;
+        if(n)
         {
-            resto = n % base;
-            cout << lista[resto];
-            n = (n - resto) / base;
+            aux = int(n);
+            n -= aux;
+            cout << aux;
         }
-        cout << lista[n] << endl << endl;
+        else
+            cout << "0";
+        cont++;
     }
 }
