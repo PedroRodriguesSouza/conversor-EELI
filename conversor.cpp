8a04008c9d6b4e7223c47f53746d4d1b9bc6ecd0
Pedro Rodrigues Souza <55005350+PedroRodriguesSouza@users.noreply.github.com>
2022-04-11 19:31:10 -0300

    corrigindo erro no float hexadecimal

--- conversor.cpp
+++ conversor.cpp
@@ -2,22 +2,21 @@
 
 using namespace std;
 
-void conversor_generico(float n, int base);
-void conversor_float(float n, int base);
+void conversor_generico(float, int, int);
+void conversor_float(float, int, int);
 
 int main()
 {
-    float n;
-
+    float n, base = 2, precisao = 8;
     cout << "Digite um numero: ";
     cin >> n;
 
-    conversor_generico(n,2);
+    conversor_generico(n, base, precisao);
 
     return 0;
 }
 
-void conversor_generico(float n, int base)
+void conversor_generico(float n, int base = 2, int precisao = 8)
 {
     int resto;
     float f = n - int(n);
@@ -28,7 +27,8 @@ void conversor_generico(float n, int base)
         resto = i % base;
         i = (i - resto) / base;
         if(i)
-            conversor_generico(i, base);
+            conversor_generico(i, base, 8);
+
         cout << resto;
     }
 
@@ -40,33 +40,58 @@ void conversor_generico(float n, int base)
 
         if(i)
             conversor_generico(n, base);
-            
+
         cout << lista[resto];
     }
 
     if(f)
     {
         cout << ",";
-        conversor_float(f, base);
+        conversor_float(f, base, precisao);
         cout << "\n\n";
     }
 }
 
-void conversor_float(float n, int base)
+void conversor_float(float n, int base = 2, int precisao = 8)
 {
     int aux;
     int cont = 0;
-    while(n != 0 && cont < 8)
+
+    if(base < 11)
     {
-        n *= base;
-        if(n)
+        while(n != 0 && cont < precisao)
         {
-            aux = int(n);
-            n -= aux;
-            cout << aux;
+            n *= base;
+            if(n)
+            {
+                aux = int(n);
+                n -= aux;
+                cout << aux;
+            }
+
+            else
+                cout << "0";
+            cont++;
         }
-        else
-            cout << "0";
-        cont++;
+    }
+
+    else
+    {
+        char lista[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+        while (n != 0 && cont < precisao)
+        {
+            n *= base;
+            if(n)
+            {
+                aux = int(n);
+                n -= aux;
+                cout << lista[aux];
+            }
+            else
+                cout << "0";
+            cont++;
+    
+        }
     }
 }
